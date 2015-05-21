93cb3cb843e7128e0a261193963f48c263164fd3
Chesdrik5 <catostylus05@gmail.com>
2015-05-21 10:50:59 -0500

    Potencias

--- miPrimer.php
+++ miPrimer.php
@@ -7,15 +7,18 @@
 				<center>
 				<?php
 						define(TAM,10);
+						function potencia ($v1,$v2){
+							$potencia = pow($v1,$v2);
+							return $potencia;
+						}
 						echo "<table border = 1>";
-						$n=1;
 						for ($n1=1;$n1<=TAM;$n1++){
 							if($n1%2 ==0)
 								echo"<tr bgcolor=#00FA9A>";
 							else
 								echo "<tr>";
 							for($n2=1;$n2<=TAM;$n2++){
-								echo "<td>",$n,"</td>";
+								echo "<td>".potencia($n1,$n2)."</td>";
 								$n=$n+1;
 						    }
 						    echo "</tr>";
